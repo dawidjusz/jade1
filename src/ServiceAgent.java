270c554b97424d57eb416851cfcd102f4381a19d
Dawid <dawidjusz@gmail.com>
2023-01-14 09:17:46 +0100

    zad3

--- src/ServiceAgent.java
+++ src/ServiceAgent.java
@@ -16,28 +16,18 @@ public class ServiceAgent extends Agent {
 		//service no 1
 		ServiceDescription sd1 = new ServiceDescription();
 		sd1.setType("answers");
-		sd1.setName("wordnet");
-		//service no 2
-		ServiceDescription sd2 = new ServiceDescription();
-		sd2.setType("answers");
-		sd2.setName("dictionary");
-		//service no 3
-		ServiceDescription sd3 = new ServiceDescription();
-		sd3.setType("answers");
-		sd3.setName("plfr");
+		sd1.setName("dict");
+
 		//add them all
 		dfad.addServices(sd1);
-		dfad.addServices(sd2);
-		dfad.addServices(sd3);
+
 		try {
 			DFService.register(this,dfad);
 		} catch (FIPAException ex) {
 			ex.printStackTrace();
 		}
-		
-		addBehaviour(new WordnetCyclicBehaviour(this));
+
 		addBehaviour(new DictionaryCyclicBehaviour(this));
-		addBehaviour(new DawidCyclicBehaviour(this));
 		//doDelete();
 	}
 	protected void takeDown() {
@@ -87,41 +77,6 @@ public class ServiceAgent extends Agent {
 	}
 }
 
-class WordnetCyclicBehaviour extends CyclicBehaviour
-{
-	ServiceAgent agent;
-	public WordnetCyclicBehaviour(ServiceAgent agent)
-	{
-		this.agent = agent;
-	}
-	public void action()
-	{
-		MessageTemplate template = MessageTemplate.MatchOntology("wordnet");
-		ACLMessage message = agent.receive(template);
-		if (message == null)
-		{
-			block();
-		}
-		else
-		{
-			//process the incoming message
-			String content = message.getContent();
-			ACLMessage reply = message.createReply();
-			reply.setPerformative(ACLMessage.INFORM);
-			String response = "";
-			try
-			{
-				response = agent.makeRequest("wn",content);
-			}
-			catch (NumberFormatException ex)
-			{
-				response = ex.getMessage();
-			}
-			reply.setContent(response);
-			agent.send(reply);
-		}
-	}
-}
 
 class DictionaryCyclicBehaviour extends CyclicBehaviour
 {
@@ -132,7 +87,7 @@ class DictionaryCyclicBehaviour extends CyclicBehaviour
 	}
 	public void action()
 	{
-		MessageTemplate template = MessageTemplate.MatchOntology("dictionary");
+		MessageTemplate template = MessageTemplate.MatchAll();
 		ACLMessage message = agent.receive(template);
 		if (message == null)
 		{
@@ -142,12 +97,13 @@ class DictionaryCyclicBehaviour extends CyclicBehaviour
 		{
 			//process the incoming message
 			String content = message.getContent();
+			String slownik = message.getOntology();
 			ACLMessage reply = message.createReply();
 			reply.setPerformative(ACLMessage.INFORM);
 			String response = "";
 			try
 			{
-				response = agent.makeRequest("english", content);
+				response = agent.makeRequest(slownik, content);
 			}
 			catch (NumberFormatException ex)
 			{
@@ -159,38 +115,3 @@ class DictionaryCyclicBehaviour extends CyclicBehaviour
 	}
 }
 
-class DawidCyclicBehaviour extends CyclicBehaviour
-{
-	ServiceAgent agent;
-	public DawidCyclicBehaviour(ServiceAgent agent)
-	{
-		this.agent = agent;
-	}
-	public void action()
-	{
-		MessageTemplate template = MessageTemplate.MatchOntology("plfr");
-		ACLMessage message = agent.receive(template);
-		if (message == null)
-		{
-			block();
-		}
-		else
-		{
-			//process the incoming message
-			String content = message.getContent();
-			ACLMessage reply = message.createReply();
-			reply.setPerformative(ACLMessage.INFORM);
-			String response = "";
-			try
-			{
-				response = agent.makeRequest("fd-pol-fra", content);
-			}
-			catch (NumberFormatException ex)
-			{
-				response = ex.getMessage();
-			}
-			reply.setContent(response);
-			agent.send(reply);
-		}
-	}
-}
